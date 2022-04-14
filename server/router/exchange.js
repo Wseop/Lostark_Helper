e7551e10b34ea04e54306b1e5cda37d56cc65f57
wooseop <crazyz2000@naver.com>
2022-04-14 17:28:50 +0900

    [Server] item 정보를 가져오는 url 변경 및 puppeteer browser 최적화 작업
    - item 정보 요청 시 queryString으로 요청 (개발자 도구의 network tab에서 url과 request 방식 확인)
    
    - 기존 로그인 방식으로 반복 작업 시, recaptcha가 동작하여 로그인 불가 -> 쿠키를 사용하도록 변경
      - 쿠키가 변경될 때 어떻게 반영할지 추가 작업 필요함
    
    - 하나의 browser에 여러 page(tab)을 띄울 경우 로그인 창으로 redirect되는 문제가 있었음 (쿠키 사용 때문인 것으로 추정)
      - 요청마다 browser를 새로 띄우고 하나의 요청내 item갯수에 따라 page를 띄우도록 수정 (browser당 page 갯수 분할)

--- server/router/exchange.js
+++ server/router/exchange.js
@@ -1,3 +1,6 @@
+// 참고한 코드
+// puppeteer 불필요한 리소스 차단 - https://gracefullight.dev/2019/07/29/increase-puppeteer-crawling-speed/
+
 const express = require('express');
 const router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
@@ -6,12 +9,18 @@ const cheerio = require('cheerio');
 
 require('dotenv').config();
 
-let browser;
+// value값 자동으로 갱신할 수 있는 방법?...
+const COOKIE = {
+    name: 'SUAT',
+    value: process.env.STOVE_COOKIE,
+    domain: '.onstove.com'
+};
 let db;
 let loginInfo;
 
+// TODO
+// 쿠키값 SUAT가 갱신되면 DB에 업데이트 하도록 구현? How?
 if (loginInfo == null) {
-    // DB에서 stove 로그인 정보를 가져와서 stove 로그인
     MongoClient.connect(process.env.DB_URL, (err, client) => {
         if (err) return console.log(err);
 
@@ -24,124 +33,65 @@ if (loginInfo == null) {
             .catch((err) => {
                 return console.log(err);
             });
-
-            //browser = await puppeteer.launch({headless:false});
-            browser = await puppeteer.launch();
-            let page = await browser.newPage();
-
-            // DEBUG - set viewport
-            await page.setViewport({
-                width: 1920,
-                height: 1080
-            });
-
-            await page.goto('https://member.onstove.com/auth/login?inflow_path=lost_ark&game_no=45&redirect_url=https%3a%2f%2flostark.game.onstove.com%2fmarket');
-            await page.focus('#user_id');
-            await page.keyboard.type(loginInfo.email);
-            await page.focus('#user_pwd');
-            await page.keyboard.type(loginInfo.pw);
-            await page.click('#idLogin > div.row.grid.el-actions > button');
-            // 로그인 버튼 클릭 후 완료될 때 까지 대기!
-            await page.waitForNavigation();
-
-            console.log('[EXCHANGE] : exchange login success');
         })();
     });
 }
 
 // 경매장에서 item의 최저가 탐색
 async function GetLowPrice(page, itemName) {
-    await page.waitForSelector('#txtItemName');
-    await page.focus('#txtItemName');
-    await page.keyboard.type(itemName);
-    await page.click('#btnSearch');
-    // 검색 결과 최저가로 정렬
-    await page.waitForSelector('.price-buy');
-    await page.click('#BUY_PRICE');
-    await page.waitForTimeout(300);
+    let urlAuction = 'https://lostark.game.onstove.com/Auction/GetAuctionListV2?sortOption.Sort=BUY_PRICE&sortOption.IsDesc=false';
+    let result = {};
+
+    await page.goto(`${urlAuction}&pageNo=1&itemName=${itemName}`);
+    await page.waitForSelector('.pagination__last');
     
-    let paginationIndex = 3;
-    while (true) {
-        const content = await page.content();
-        const $ = cheerio.load(content);
+    let content = await page.content();
+    let $ = cheerio.load(content);
+    const pageCount = Number($('.pagination__last').attr('onclick').split(/.*?\(|\)/)[1]);
+    const pageArr = Array.from({length: pageCount}, (v, i) => i + 1);
 
-        for (let j = 0; j < 10; j++) {
-            let selector = '#auctionListTbody > tr:nth-child(' + (j + 1) + ') > td:nth-child(6) > div > em';
-            let price = $(selector).text().trim();
+    for (let pageNo of pageArr) {
+        const url = `${urlAuction}&pageNo=${pageNo}&itemName=${itemName}`;
 
+        await page.goto(url);
+        await page.waitForSelector('#auctionListTbody');
+        content = await page.content();
+        $ = cheerio.load(content);
+        
+        const itemList = $('#auctionListTbody').children();
+        for (let item of itemList) {
+            let price = $(item).find('.price-buy').children('em').text().trim();
+            
             if (price !== '-') {
-                let result = {};
-                result['imgSrc'] = $('#auctionListTbody > tr:nth-child(' + (j + 1) + ') > td:nth-child(1) > div.grade > span.slot > img').attr('src');
-                result['name'] = $('#auctionListTbody > tr:nth-child(' + (j + 1) + ') > td:nth-child(1) > div.grade > span.name').text();
+                result['imgSrc'] = $(item).find('img').attr('src');
+                result['name'] = $(item).find('.name').text();
                 result['price'] = price;
-                result['dataGrade'] = $('#auctionListTbody > tr:nth-child(' + (j + 1) + ') > td:nth-child(1) > div.grade').attr('data-grade');
-
-                await page.click('#lostark-wrapper > div > main > div > div.deal-tab > a.tab__item--active');
+                result['dataGrade'] = $(item).find('.grade').attr('data-grade');
 
                 return result;
             }
         }
-        // 최저가를 못찾은 경우 다음 tab에서 다시 검색
-        if (paginationIndex === 12) {
-            await page.click('#auctionList > div.pagination > a.pagination__next');
-            paginationIndex = 3;
-        } else {
-            paginationIndex++;
-
-            let selector = '#auctionList > div.pagination > a:nth-child(' + (paginationIndex) + ')';
-            if (await page.$(selector) != null) {
-                await page.click(selector);
-            } else {
-                // 검색 결과에 최저가 즉구가 없는 경우
-                await page.click('#lostark-wrapper > div > main > div > div.deal-tab > a.tab__item--active');
-
-                return null;
-            }
-        }
-        await page.waitForTimeout(300);
     }
+    result = null;
+
+    return result;
 }
 
 // 거래소에서 item 최저가 탐색
-async function GetPrice(itemName) {
-    let page = await browser.newPage();
+async function GetPrice(page, itemName) {
+    let url = `https://lostark.game.onstove.com/Market/List_v2?firstCategory=0&secondCategory=0&tier=0&grade=99&pageNo=1&isInit=false&sortType=7&itemName=${itemName}`;
 
-    await page.setRequestInterception(true);
-    page.on('request', (request) => {
-        switch (request.resourceType()) {
-            case 'stylesheet':
-            case 'font':
-            case 'image':
-                request.abort();
-                break;
-            default:
-                request.continue();
-                break;
-        }
-    });
-
-    // TODO
-    // Market 검색은 아래 경로 활용하도록 코드 수정
-    // https://lostark.game.onstove.com/Market/List_v2
-
-    await page.goto('https://lostark.game.onstove.com/Market');
-
-    await page.waitForSelector('#txtItemName');
-    await page.focus('#txtItemName');
-    await page.keyboard.type(itemName);
-    await page.click('#lostark-wrapper > div > main > div > div.deal > div.deal-contents > form > fieldset > div > div.bt > button.button.button--deal-submit');
-    await page.waitForSelector('.price');
-
+    await page.goto(url);
+    await page.waitForSelector('#tbodyItemList');
+    
     const content = await page.content();
     const $ = cheerio.load(content);
     let result = {};
 
     result['imgSrc'] = $('#tbodyItemList > tr:nth-child(1) > td:nth-child(1) > div > span.slot > img').attr('src');
     result['name'] = $('#tbodyItemList > tr:nth-child(1) > td:nth-child(1) > div > span.name').text();
-    result['price'] = $('#tbodyItemList > tr:nth-child(1) > td:nth-child(4) > div > em').text().trim();
+    result['price'] = $('#tbodyItemList > tr:nth-child(1) > td:nth-child(4) > div > em').text();
     result['dataGrade'] = $('#tbodyItemList > tr:nth-child(1) > td:nth-child(1) > div').attr('data-grade');
-
-    await page.close();
     
     return result;
 }
@@ -149,12 +99,10 @@ async function GetPrice(itemName) {
 // 경매장 데이터 전달
 router.get('/auction', (req, res) => {
     (async() => {
-        let items = req.query.items;
-        let results = [];
-        let page = await browser.newPage();
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
 
         // 불필요한 리소스 차단
-        // 참고) https://gracefullight.dev/2019/07/29/increase-puppeteer-crawling-speed/
         await page.setRequestInterception(true);
         page.on('request', (request) => {
             switch (request.resourceType()) {
@@ -168,56 +116,58 @@ router.get('/auction', (req, res) => {
                     break;
             }
         });
+        await page.setCookie(COOKIE);
 
-        // TODO
-        // Auction 검색은 아래 경로 활용하도록 코드 수정
-        // https://lostark.game.onstove.com/Auction/GetAuctionListV2/
-
-        await page.goto('https://lostark.game.onstove.com/Auction');
+        let items = req.query.items;
+        let results = [];
 
         for (let item of items) {
             let result = await GetLowPrice(page, item);
-            results.push(result);
+            if (result != null) {
+                results.push(result);
+            }
         }
 
         await page.close();
+        await browser.close();
         res.send(results);
     })();
 });
 
-// 병렬 처리
-// 참고) https://jun-choi-4928.medium.com/puppeteer-%ED%81%AC%EB%A1%A4%EB%A7%81-%EB%B9%84%EB%8F%99%EA%B8%B0-%EB%B3%91%EB%A0%AC%EC%B2%98%EB%A6%AC%ED%95%98%EA%B8%B0-4bee1e5af998
-function arrayToChunks (array, CHUNK_SIZE) {
-    const results = [];
-    let start = 0;
-
-    while (start < array.length) {
-        results.push(array.slice(start, start + CHUNK_SIZE));
-        start += CHUNK_SIZE;
-    }
-
-    return results;
-}
-
 // 거래소 데이터 전달
 router.get('/market', (req, res) => {
     (async() => {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+
+        // 불필요한 리소스 차단
+        await page.setRequestInterception(true);
+        page.on('request', (request) => {
+            switch (request.resourceType()) {
+                case 'stylesheet':
+                case 'font':
+                case 'image':
+                    request.abort();
+                    break;
+                default:
+                    request.continue();
+                    break;
+            }
+        });
+        await page.setCookie(COOKIE);
+
         let items = req.query.items;
         let results = [];
-        const CHUNK_SIZE = 4;
-        const chunkedItems = arrayToChunks(items, CHUNK_SIZE);
 
-        // 병렬 처리
-        // 참고) https://jun-choi-4928.medium.com/puppeteer-%ED%81%AC%EB%A1%A4%EB%A7%81-%EB%B9%84%EB%8F%99%EA%B8%B0-%EB%B3%91%EB%A0%AC%EC%B2%98%EB%A6%AC%ED%95%98%EA%B8%B0-4bee1e5af998
-        for (chunk of chunkedItems) {
-            const resultPromises = chunk.map((item) => GetPrice(item));
-            const resolved = await Promise.all(resultPromises);
-            
-            resolved.forEach((result) => {
-                result && results.push(result);
-            });
+        for (let item of items) {
+            let result = await GetPrice(page, item);
+            if (result != null) {
+                results.push(result);
+            }
         }
 
+        await page.close();
+        await browser.close();
         res.send(results);
     })();
 });
